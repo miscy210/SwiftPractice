23e55f9e0bd699c61ea84da0bd1cbb2e42ecf493
Bin Mao <miscy210@163.com>
2018-01-14 19:17:46 +0800

    Update ViewController.swift
    swift4 上的调试。差别有五个地方：
    1）weight: UIFont.Weight(rawValue: YHScreenWidth/4)。（源于报错：Cannot convert value of type 'CGFloat' to expected argument type 'UIFont.Weight'）
    2） @objc修饰符，可以直接供 Objective-C 调用。（源于xcode的报错：Argument of '#selector' refers to instance method 'buttonTapped(sender:)' that is not exposed to Objective-C）
    3）line55-60的一个逻辑。（源于：当没启动watch时，暂停/继续按钮应当设为无效。）
    4）line74-75的设定。（源于：停止键需要重设状态。）
    5）一个小调整把contin改成resume。

--- Project01_SimpleClock/SimpleClock/ViewController.swift
+++ Project01_SimpleClock/SimpleClock/ViewController.swift
@@ -9,92 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     
-    let showLable = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: YHScreenWidth, height: YHScreenHeight/2))
-    let beign = UIButton(frame: CGRect(x: 0.0, y: YHScreenHeight/2, width: YHScreenWidth/2, height: YHScreenHeight/2))
+    let showLabel = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: YHScreenWidth, height: YHScreenHeight/2))
+    let begin = UIButton(frame: CGRect(x: 0.0, y: YHScreenHeight/2, width: YHScreenWidth/2, height: YHScreenHeight/2))
     let pause = UIButton(frame: CGRect(x: YHScreenWidth/2, y: YHScreenHeight/2, width: YHScreenWidth/2, height: YHScreenHeight/2))
     var time : Timer?
     var n = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white//背景色
+        view.backgroundColor = .white
         
-        //添加界面
         setupView()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     func setupView(){
-        showLable.backgroundColor = .yellow
-        beign.backgroundColor = .orange
+        showLabel.backgroundColor = .yellow
+        begin.backgroundColor = .orange
         pause.backgroundColor = .blue
-        beign.setTitle("开始", for: .normal)
-        beign.setTitle("结束", for: .selected)
-        pause.setTitle("暂停", for: .normal)
-        pause.setTitle("继续", for: .selected)
-        [beign,pause].forEach{
+        begin.setTitle("Begin", for: .normal)
+        begin.setTitle("Stop", for: .selected)
+        pause.setTitle("Pause", for: .normal)
+        pause.setTitle("Resume", for: .selected)
+        [begin, pause].forEach{
             ($0.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside))
         }
-        /*
-         bumbers.sort{(first,second) in 
-            return first > second
-         }
-         等价
-         bumbers.sort{$0 > $1}
-         */
-        showLable.text = "0"
-        showLable.textColor = .white
-        showLable.font = UIFont.systemFont(ofSize: YHScreenHeight/4, weight: YHScreenWidth/4)
-        showLable.textAlignment = .center
-        view.addSubview(showLable)
-        view.addSubview(beign)
+        
+        showLabel.text = "0"
+        showLabel.textColor = .white
+        showLabel.font = UIFont.systemFont(ofSize: YHScreenHeight/4, weight: UIFont.Weight(rawValue: YHScreenWidth/4))
+        showLabel.textAlignment = .center
+        view.addSubview(showLabel)
+        view.addSubview(begin)
         view.addSubview(pause)
     }
     
-    func buttonTapped(sender: UIButton) {
-        switch sender {
-        case beign:
-            beign.isSelected = !beign.isSelected
-            beign.isSelected ? beginSC() : stopSC()
+    @objc func buttonTapped(sender:UIButton){
+        switch sender{
+        case begin:
+            begin.isSelected = !begin.isSelected
+            begin.isSelected ? beginSC() : stopSC()
         case pause:
-            pause.isSelected = !pause.isSelected
-            pause.isSelected ? pauseSC() : continSC()
-            
+            if begin.isSelected{
+                pause.isSelected = !pause.isSelected
+                pause.isSelected ? pauseSC() : resumeSC()
+            }else{
+                return
+            }
         default:
             break
         }
     }
     
-    // MARK: - 操作
-    // TODO: - 记得做
-    // FIXME: - 提醒
-    
-    func beginSC() {
+    func beginSC(){
         time = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeLabel), userInfo: nil, repeats: true)
     }
-    func stopSC() {
-        showLable.text = "0"
+    
+    func stopSC(){
+        showLabel.text = "0"
         time?.invalidate()
         time = nil
+        n = 0
+        pause.isSelected = false
     }
-    func pauseSC() {
-        if !beign.isSelected {
+    
+    func pauseSC(){
+        if !begin.isSelected{
             return
         }
         time?.invalidate()
         time = nil
     }
-    func continSC() {
-        if !beign.isSelected {
+    
+    func resumeSC(){
+        if !begin.isSelected{
             return
         }
         beginSC()
     }
-    func changeLabel() {
+    
+    @objc func changeLabel(){
         n += 1
-        showLable.text = String(n)
-        //s = "\(x)" | toString(x) | x.description
+        showLabel.text = String(n)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
